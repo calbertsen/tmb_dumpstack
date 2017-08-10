f31785c0dd1094504f0d77dbda4c1c7fa878f767
Kasper Kristensen <kaskr@dtu.dk>
2017-08-10 14:12:59 +0200

    update

--- dumpstack.R
+++ dumpstack.R
@@ -5,52 +5,63 @@
 ## ========================
 library(TMB)
 compile("examp.cpp", libtmb=FALSE)
-dyn.load("examp.so")
-obj <- MakeADFun(data=list(),parameters=list(u=rep(0,8)),type=c("ADFun","ADGrad"))
+dyn.load(dynlib("examp"))
+obj <- MakeADFun(data=list(),
+                 parameters=list(u=rep(0,8)),
+                 type=c("ADFun","ADGrad"))
 
 sink("value")
-qw <- obj$env$f(dumpstack=TRUE)
+obj$env$f(dumpstack=TRUE)
 sink()
+
 sink("grad")
-qw <- obj$env$f(type="ADGrad",dumpstack=TRUE)
+obj$env$f(type="ADGrad", dumpstack=TRUE)
 sink()
-makeDot <- function(file,bold=NULL,filled=NULL,dotfile=paste0(file,".dot"),svgfile=paste0(file,".svg"),
+
+makeDot <- function(file,
+                    bold=NULL,
+                    filled=NULL,
+                    dotfile=paste0(file,".dot"),
+                    svgfile=paste0(file,".svg"),
                     forward=TRUE){
-  d <- readLines(file)
-  d <- gsub(" +"," ",d)
-  d <- strsplit(d," ")
-  node <- sub("i=","",sapply(d,function(x)x[1]))
-  op <- sub("op=","",sapply(d,function(x)x[2]))
-  parseDep <- function(x){
-    ans <- sub(".*=","",grep("v.=|.v=",x,value=TRUE))  
-    ##unique(ans)
-    ans
-  }
-  args <- lapply(d,parseDep)
-  if(!forward){
-      edges <- unlist(Map(paste,node,args,sep=" -> ")[sapply(args,length)>0])
-  } else {
-      edges <- unlist(Map(paste,args,node,sep=" -> ")[sapply(args,length)>0])
-  }
-  lab <- paste(op,node)
-  labels <- paste0(node," [label=\"",lab,"\"]")
-  labels <- labels[op!="End"]
-  if(!is.null(bold))bold <- paste(bold,"[style=dashed]")
-  if(!is.null(filled))filled <- paste(filled,"[style=filled]")
-  group <- function(x,rank="same")c("{",paste0("rank=",rank),x,"}")
-  spl <- split(node[op!="End"],op[op!="End"])
-  graph <- c(
-             "digraph graphname {",
-             unlist(lapply(spl,group)),
-             edges,
-             labels,
-             filled,
-             bold,
-             "}")
-  cat("Writing",dotfile,"\n")  
-  writeLines(graph,dotfile)
-  cat("Writing",svgfile,"\n")
-  system(paste("dot -Tsvg",dotfile,">",svgfile))
+    d <- readLines(file)
+    d <- d[d != ""]
+    d <- sub("^o=[^ ]*[ ]*","",d)
+    d <- gsub(" +"," ",d)
+    d <- strsplit(d," ")
+    node <- sub("i=","",sapply(d,function(x)x[1]))
+    node <- sub("v=", "", node)
+    op <- sub("op=","",sapply(d,function(x)x[2]))
+    parseDep <- function(x){
+        ans <- sub(".*=","",grep("v.=|.v=",x,value=TRUE))
+        ##unique(ans)
+        ans
+    }
+    args <- lapply(d,parseDep)
+    if(!forward){
+        edges <- unlist(Map(paste,node,args,sep=" -> ")[sapply(args,length)>0])
+    } else {
+        edges <- unlist(Map(paste,args,node,sep=" -> ")[sapply(args,length)>0])
+    }
+    lab <- paste(op,node)
+    labels <- paste0(node," [label=\"",lab,"\"]")
+    labels <- labels[op!="End"]
+    if(!is.null(bold))bold <- paste(bold,"[style=dashed]")
+    if(!is.null(filled))filled <- paste(filled,"[style=filled]")
+    group <- function(x,rank="same")c("{",paste0("rank=",rank),x,"}")
+    spl <- split(node[op!="End"],op[op!="End"])
+    graph <- c(
+        "digraph graphname {",
+        unlist(lapply(spl,group)),
+        edges,
+        labels,
+        filled,
+        bold,
+        "}")
+    cat("Writing",dotfile,"\n")
+    writeLines(graph,dotfile)
+    cat("Writing",svgfile,"\n")
+    system(paste("dot -Tsvg",dotfile,">",svgfile))
 }
 
 makeDot("grad",bold=c(seq(16,28,by=2),29),filled=c(2,3,4,10,11,25,23,26))
