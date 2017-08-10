4a955e7d686d115a78193329fd52bd138a4d3077
Kasper Kristensen <kaskr@dtu.dk>
2017-08-10 14:46:48 +0200

    re-structure

--- dumpstack.R
+++ dumpstack.R
@@ -4,6 +4,8 @@
 ## apt-get install inkscape
 ## ========================
 library(TMB)
+source("makeDot.R")
+
 compile("examp.cpp", libtmb=FALSE)
 dyn.load(dynlib("examp"))
 obj <- MakeADFun(data=list(),
@@ -18,52 +20,6 @@ sink()
 sink("grad")
 dummy <- obj$env$f(type="ADGrad", dumpstack=TRUE)
 sink()
-
-makeDot <- function(file,
-                    bold=NULL,
-                    filled=NULL,
-                    dotfile=paste0(file,".dot"),
-                    svgfile=paste0(file,".svg"),
-                    forward=TRUE){
-    d <- readLines(file)
-    d <- d[d != ""]
-    d <- sub("^o=[^ ]*[ ]*","",d)
-    d <- gsub(" +"," ",d)
-    d <- strsplit(d," ")
-    node <- sub("i=","",sapply(d,function(x)x[1]))
-    node <- sub("v=", "", node)
-    op <- sub("op=","",sapply(d,function(x)x[2]))
-    parseDep <- function(x){
-        ans <- sub(".*=","",grep("v.=|.v=",x,value=TRUE))
-        ##unique(ans)
-        ans
-    }
-    args <- lapply(d,parseDep)
-    if(!forward){
-        edges <- unlist(Map(paste,node,args,sep=" -> ")[sapply(args,length)>0])
-    } else {
-        edges <- unlist(Map(paste,args,node,sep=" -> ")[sapply(args,length)>0])
-    }
-    lab <- paste(op,node)
-    labels <- paste0(node," [label=\"",lab,"\"]")
-    labels <- labels[op!="End"]
-    if(!is.null(bold))bold <- paste(bold,"[style=dashed]")
-    if(!is.null(filled))filled <- paste(filled,"[style=filled]")
-    group <- function(x,rank="same")c("{",paste0("rank=",rank),x,"}")
-    spl <- split(node[op!="End"],op[op!="End"])
-    graph <- c(
-        "digraph graphname {",
-        unlist(lapply(spl,group)),
-        edges,
-        labels,
-        filled,
-        bold,
-        "}")
-    cat("Writing",dotfile,"\n")
-    writeLines(graph,dotfile)
-    cat("Writing",svgfile,"\n")
-    system(paste("dot -Tsvg",dotfile,">",svgfile))
-}
 
 makeDot("grad",bold=c(seq(16,28,by=2),29),filled=c(2,3,4,10,11,25,23,26))
 makeDot("value")##,bold=24)
